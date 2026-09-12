af625030d73184a4ed2df94c83f517af8600b139
jqang-gif <jqang@carc.pshs.edu.ph>
2026-09-12 21:53:50 +0800

    Update RPGHeroBerylliumAng.py
    Added comments to clarify class and method functionality.

--- RPGHeroBerylliumAng.py
+++ RPGHeroBerylliumAng.py
@@ -1,19 +1,20 @@
-class Hero: #HeroName
+class Hero: #Class Name
     def __init__(self, heroname, hp):
 
-        self.heroname = heroname
-        self.hp = hp
+        self.heroname = heroname #Hero name
+        self.hp = hp #Hero health
 
     def stats(self):
+        #How it will print when called
         return f"{self.heroname} : {self.hp}"
 
     def take_damage(self, damageamount):
-        self.hp = self.hp - damageamount
+        self.hp = self.hp - damageamount #Damage to Hero health
 
-hero1 = Hero("Arthur", 100)
-hero2 = Hero("Morgana", 100)
+hero1 = Hero("Arthur", 100) #Arthur Stats
+hero2 = Hero("Morgana", 100) #Morgana Stats
 
-hero1.take_damage(10)
+hero1.take_damage(10) #Damage to Arthur's hp
 
-print(hero1.stats())
-print(hero2.stats())
+print(hero1.stats()) #Print Arthur's stats
+print(hero2.stats()) #Print Morgana's stats
